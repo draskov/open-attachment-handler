2c8d2f8c518f09869d3b26dfb959b4c2b0e77d87
Drasko Vrucinic <drasko.vrucinic@computerrock.com>
2024-12-30 23:55:32 +0100

    clean up finished

--- src/main/java/rs/pumpkin/open_attachment_handler/model/AttachmentParams.java
+++ src/main/java/rs/pumpkin/open_attachment_handler/model/AttachmentParams.java
@@ -3,5 +3,4 @@ package rs.pumpkin.open_attachment_handler.model;
 import lombok.Builder;
 
 @Builder
-public record AttachmentParams(String url, String id) {
-}
+public record AttachmentParams(String url, String id) {}
